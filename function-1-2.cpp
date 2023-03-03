f6c4b9e412462afa5f961a329eecefed35bc8977
WillarDraco <a1863235@adelaide.edu.au>
2023-03-03 14:26:58 +1030

    Updated function-1-2.cpp and main-1-2.cpp

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -4,16 +4,13 @@ int array_mean(int array[], int n) {
     int sum = 0;
 
     if (n < 1) {
-        return 0.0;
+        return 0;
     }
 
     for (int i = 0; i < n; i++) {
         sum = sum + array[i];
     }
     
-    sum = double(sum);
-    n = double(n);
-
     double average = sum/n;
 
     return average;
