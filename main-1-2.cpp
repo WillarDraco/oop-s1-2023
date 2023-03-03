918ac1b675b566ad6ea2275731c781c30196d17b
WillarDraco <a1863235@adelaide.edu.au>
2023-03-03 14:31:17 +1030

    Updated function-1-2.cpp and main-1-2.cpp

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -3,9 +3,9 @@
 extern double array_mean(int array[], int n);
 
 int main(void) {
-    int array[5] = {1,2,3,4,5};
+    int array[5] = {2,2,2,2,2};
     int n = sizeof(array)/sizeof(int);
 
-    double average = array_mean(array, n);
-    printf("The average is %0.02f. \n", average);
+    double mean = array_mean(array, n);
+    printf("The average is %0.02f. \n", mean);
 }
