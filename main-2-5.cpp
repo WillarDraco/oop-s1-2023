e4d97f394668141b2ab4c86b358f05ae1eccc84d
WillarDraco <a1863235@adelaide.edu.au>
2023-03-05 20:49:00 +1030

    Created function-2-5.cpp and main-2-5.cpp

---
+++ main-2-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+extern bool is_descending(int[], int);
+
+int main(void) {
+    int array[] = {4,3,2,1};
+    int n = sizeof(array)/sizeof(int);
+
+    int descending = is_descending(array, n);
+
+    if (descending == true) {
+        printf("The array is descending.\n");
+    } else if (descending == false) {
+        printf("The array is not dscending.\n");
+    }
+}
