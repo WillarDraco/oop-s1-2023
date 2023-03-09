bbf1d0db85437bc76e3a6ab93baf20095ad253b8
WillarDraco <a1863235@adelaide.edu.au>
2023-03-09 17:03:40 +1030

    Edited function-2-3.cpp to include palindrome.cpp and sumarray.cpp

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,8 +1,55 @@
 #include <iostream>
 #include <math.h>
 
-extern bool is_palindrome(int integers[], int length);
-extern int sum_array_elements(int integers[], int length);
+bool is_palindrome(int integers[], int length) {
+
+    bool pal = false;
+
+    if (length < 1) {
+        pal = false;
+        return pal;
+    }
+
+    for (int i = 0; i < length; i++) {
+        if (integers[i] == integers[length - 1 - i]) {
+            pal = true;
+        } else {
+            pal = false;
+            return pal;
+        }
+    }
+
+
+    if (length % 2 == 0) {
+        length = length;
+    } else {
+        length = length + 1;
+    }
+
+    for (int j = 1; j < (length / 2); j++) {
+        if (integers[j - 1] <= integers[j]) {
+            pal = true;
+        } else {
+            pal = false;
+            return pal;
+        }
+    }
+
+    return pal;
+}
+
+int sum_array_elements(int integers[], int length) {
+    int sum = 0;
+
+    if (length < 1) {
+        return 0;
+    }
+
+    for (int i = 0; i < length; i++) {
+        sum = sum + integers[i];
+    }
+    return sum;
+}
 
 int sum_if_palindrome(int integers[], int length) {
     bool pal = is_palindrome(integers, length);
