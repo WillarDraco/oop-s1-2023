1f6d72d5c95354416f580332fc9816feae553da8
WillarDraco <a1863235@adelaide.edu.au>
2023-03-17 13:19:09 +1030

    Created function-2-2.cpp and main-2-2.cpp

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
+# include <iostream>
 
-extern int binary_to_int(int binary_digits[], int number_of_digits);
+extern int bin_to_int(int binary_digits[], int number_of_digits);
 
 int main(void) {
     int binary_digits[] = {1,0,1,1};
-    int number_of_digits = sizeof(binary_digits) / sizeof(int);
-    int number = binary_to_int(binary_digits, number_of_digits);
+    int number_of_digits = sizeof(binary_digits)/sizeof(int);
 
-    printf("%d\n", number);
+    int decimal = bin_to_int(binary_digits, number_of_digits);
+
+    printf("%d\n", decimal);
 }
