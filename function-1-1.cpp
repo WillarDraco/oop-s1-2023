5cc1cb686dbf3a963495ed6f0ab2f0942f7d0d52
WillarDraco <a1863235@adelaide.edu.au>
2023-03-21 10:54:51 +1030

    Edited function-1-1.cpp and main-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -20,4 +20,5 @@ void printNumbers(int *numbers,int length) {
     for (int i = 0; i < length; i++) {
         std::cout << i << " " << numbers[i] << std::endl;
     }
+    delete(numbers);
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -4,8 +4,6 @@ extern int *readNumbers();
 extern void printNumbers(int *numbers, int length);
 
 int main(void) {
-    int *numbers = readNumbers();
     int length = 10;
-    printNumbers(numbers, length);
-    delete(numbers);
+    printNumbers(readNumbers(), length);
 }
