786dc4c146336e59512c8bbfc252e7b24484beaa
WillarDraco <a1863235@adelaide.edu.au>
2023-03-21 10:48:53 +1030

    Edited function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -13,6 +13,10 @@ int *readNumbers(void) {
 }
 
 void printNumbers(int *numbers,int length) {
+    if (length <= 0) {
+        return;
+    }
+    
     for (int i = 0; i < length; i++) {
         std::cout << i << " " << numbers[i] << std::endl;
     }
