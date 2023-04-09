e79baf988deb11bc125ca3f5632cf4098e692ef7
WillarDraco <a1863235@adelaide.edu.au>
2023-04-09 23:43:38 +0930

    edited main-1-2.cpp

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -7,39 +7,39 @@
 
 int main() {
 
-ParkingLot p1(10);
-std::string vehicleType;
+    ParkingLot p1(10);
+    std::string vehicleType;
 
-std::cout << "What type of vehicle is parking" << std::endl;
-std::cin >> vehicleType;
+    std::cout << "What type of vehicle is parking" << std::endl;
+    std::cin >> vehicleType;
 
-bool full = false;
-int index = 0;
-while (full == false) {
-    if (vehicleType == "Car") {
-        Car newVehicle = Car(index);
-        p1.parkVehicle(&newVehicle);
-        index++;
-        std::cout << "Parked a car" << std::endl;
-    } else if (vehicleType == "Bus") {
-        Bus newVehicle = Bus(index);
-        std::cout << "Parked a bus" << std::endl;
-        p1.parkVehicle(&newVehicle);
-        index++;
-    } else if (vehicleType == "Motorbike") {
-        Motorbike newVehicle = Motorbike(index);
-        std::cout << "Parked a motorbike" << std::endl;
-        p1.parkVehicle(&newVehicle);
-        index++;
+    bool full = false;
+    int index = 0;
+    while (full == false) {
+        if (vehicleType == "Car") {
+            Car newVehicle = Car(index);
+            p1.parkVehicle(&newVehicle);
+            index++;
+            std::cout << "Parked a car" << std::endl;
+        } else if (vehicleType == "Bus") {
+            Bus newVehicle = Bus(index);
+            std::cout << "Parked a bus" << std::endl;
+            p1.parkVehicle(&newVehicle);
+            index++;
+        } else if (vehicleType == "Motorbike") {
+            Motorbike newVehicle = Motorbike(index);
+            std::cout << "Parked a motorbike" << std::endl;
+            p1.parkVehicle(&newVehicle);
+            index++;
+        }
+        if (p1.getCount() == 10) {
+            full = true;
+        } 
     }
-    if (p1.getCount() == 10) {
-        full = true;
-    } 
-}
 
-int unPark = 0;
-std::cout << "Enter an ID to remove" << std::endl;
-std::cin >> unPark;
-p1.unparkVehicle(unPark);
+    int unPark = 0;
+    std::cout << "Enter an ID to remove" << std::endl;
+    std::cin >> unPark;
+    p1.unparkVehicle(unPark);
 
 }
