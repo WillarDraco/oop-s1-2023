aea9ca9d5566b9da075f089c659f7498d4881686
WillarDraco <a1863235@adelaide.edu.au>
2023-04-09 23:39:47 +0930

    Edited main-1-1.cpp and main-1-2.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
-#include <string>
-#include <time.h>
-#include "Motorbike.h"
+#include "Vehicle.h"
 #include "Car.h"
 #include "Bus.h"
-using namespace std;
+#include "Motorbike.h"
 
-int main(void) {
-    Car c1;
+int main() {
+
+    Vehicle v1 = Vehicle(1);
+    int vehicleCount = 0;
+    int carCount = 0;
+    int busCount = 0;
+    int motorbikeCount = 0;
+
+    std::cout << "How many cars" << std::endl;
+    std::cin >> carCount;
+    std::cout << "How many busses" << std::endl;
+    std::cin >> busCount;
+    std::cout << "How many motorbikes" << std::endl;
+    std::cin >> motorbikeCount;
+
+    vehicleCount = carCount + busCount + motorbikeCount;
+
+    Vehicle *array[vehicleCount];
+
+    for (int i = 0; i < vehicleCount; i++) {
+        if (i < carCount) {
+            array[i] = new Car(i+1);
+            std::cout << "Added car" << std::endl;
+        } else if ( i < busCount + carCount) {
+            array[i] = new Bus(i+1);
+            std::cout << "Added bus" << std::endl;
+        } else if (i < motorbikeCount + busCount + carCount) {
+            array[i] = new Motorbike(i+1);
+            std::cout << "Added motorbike" << std::endl;
+        }
+    }
+
+    
+    for (int i = 0; i < vehicleCount; i++) {
+        if (i < carCount) {
+            std::cout << "Duration: " << array[i]->getParkingDuration() << std::endl;
+            std::cout << "ID: " << array[i]->getID() << std::endl;
+        } else if ( i < busCount + carCount) {
+            std::cout << "Duration:L " << array[i]->getParkingDuration() << std::endl;
+            std::cout << "ID: " << array[i]->getID() << std::endl;
+        } else if (i < motorbikeCount +carCount + busCount) {
+            std::cout << "Duration: " << array[i]->getParkingDuration() << std::endl;
+            std::cout << "ID: " << array[i]->getID() << std::endl;
+        }
+    }
 }
--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
 #include "Vehicle.h"
-#include "ParkingLot.h"
 #include "Car.h"
 #include "Bus.h"
 #include "Motorbike.h"
+#include "ParkingLot.h"
 
 int main() {
 
 ParkingLot p1(10);
+std::string vehicleType;
+
+std::cout << "What type of vehicle is parking" << std::endl;
+std::cin >> vehicleType;
+
+bool full = false;
+int index = 0;
+while (full == false) {
+    if (vehicleType == "Car") {
+        Car newVehicle = Car(index);
+        p1.parkVehicle(&newVehicle);
+        index++;
+        std::cout << "Parked a car" << std::endl;
+    } else if (vehicleType == "Bus") {
+        Bus newVehicle = Bus(index);
+        std::cout << "Parked a bus" << std::endl;
+        p1.parkVehicle(&newVehicle);
+        index++;
+    } else if (vehicleType == "Motorbike") {
+        Motorbike newVehicle = Motorbike(index);
+        std::cout << "Parked a motorbike" << std::endl;
+        p1.parkVehicle(&newVehicle);
+        index++;
+    }
+    if (p1.getCount() == 10) {
+        full = true;
+    } 
+}
+
+int unPark = 0;
+std::cout << "Enter an ID to remove" << std::endl;
+std::cin >> unPark;
+p1.unparkVehicle(unPark);
+
 }
