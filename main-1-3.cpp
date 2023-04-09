12e825924ecc097fb54e471d2d1bb3c82bf1f2e7
WillarDraco <a1863235@adelaide.edu.au>
2023-04-09 23:47:28 +0930

    Edited main-1-3.cpp

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -7,5 +7,37 @@
 
 int main() {
 
-ParkingLot p1(10);
+    ParkingLot p1(10);
+    std::string vehicleType;
+
+    bool full = false;
+    int index = 0;
+    while (full == false) {
+        std::cout << "What kind of vehicle would you like to park? " << std::endl;
+        std::cin >> vehicleType;
+
+        if (vehicleType == "Car") {
+            Car newVehicle(index);
+            std::cout << newVehicle.getID() << std::endl;
+            p1.parkVehicle(&newVehicle);
+            index++;
+            std::cout << "Parked a car" << std::endl;
+        } else if (vehicleType == "Bus") {
+            Bus newVehicle(index);
+            std::cout << "Parked a bus" << std::endl;
+            p1.parkVehicle(&newVehicle);
+            index++;
+        } else if (vehicleType == "Motorbike") {
+            Motorbike newVehicle(index);
+            std::cout << "Parked a motorbike" << std::endl;
+            p1.parkVehicle(&newVehicle);
+            index++;
+        }
+        if (p1.getCount() == 10) {
+            full = true;
+        } 
+    }
+
+    int carsExceeded = p1.countOverstayingVehicles(15);
+    std::cout << carsExceeded << "Cars have stayed for longer than 15 seconds" << std::endl;
 }
