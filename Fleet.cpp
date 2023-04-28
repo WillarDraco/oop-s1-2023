ab03308eca7beb2a719b686d09597ee8fb082136
WillarDraco <a1863235@adelaide.edu.au>
2023-04-28 14:31:27 +0930

    Edited fleet.cpp

--- Fleet.cpp
+++ Fleet.cpp
@@ -11,11 +11,11 @@ Fleet::Fleet() {
     Ford f2(2, 40000); 
     Tesla T('T', 50000);
 
-    fleet[1] = &S;
-    fleet[2] = &f1;
-    fleet[3] = &car;
-    fleet[4] = &f2;
-    fleet[5] = &T;
+    fleet[0] = &S;
+    fleet[1] = &f1;
+    fleet[2] = &car;
+    fleet[3] = &f2;
+    fleet[4] = &T;
 }
 
 Car **Fleet::get_fleet() {
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -3,5 +3,5 @@
 
 int main(void) {
     Fleet f1;
-    std::cout << f1.get_fleet()[5]->get_price();
+    std::cout << f1.get_fleet()[0]->get_price();
 }
