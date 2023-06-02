fa32780bacfb8758f93faa0b4f08c0feff80c1fa
willardraco <a1863235@adelaide.edu.au>
2023-06-02 16:54:19 +0930

    Mdee main 3

---
+++ main-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <tuple>
+#include <cmath>
+#include "Spot.h"
+#include "Assists.h"
+#include "Snare.h"
+#include "Persona.h"
+#include <vector>
+#include "Play.h"
+
+int main(void) {
+    Spot spot(0,0);
+}
