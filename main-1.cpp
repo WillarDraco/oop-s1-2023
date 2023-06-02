60f002cf6b06d7f2c6780c40afffc1f3789441ed
willardraco <a1863235@adelaide.edu.au>
2023-06-02 15:33:22 +0930

    Static

--- main-1.cpp
+++ main-1.cpp
@@ -6,7 +6,6 @@
 
 int main(void) {
     Spot spot1(0, 0, 'S');
-    Assists assists;
-    std::tuple<int, int> loc2 = assists.createRandomLoc(10,10);
-    std::cout << assists.evaluateDistance(loc2, spot1.getLoc());
+    std::tuple<int, int> loc2 = Assists::createRandomLoc(10,10);
+    std::cout << Assists::evaluateDistance(loc2, spot1.getLoc());
 }
