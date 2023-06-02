d840da2b412847f0b7fe4b369648ecc1578675f2
willardraco <a1863235@adelaide.edu.au>
2023-06-02 16:55:59 +0930

    Edit

--- Assists.h
+++ Assists.h
@@ -10,8 +10,6 @@ class Assists {
     public:
         static std::tuple<int, int> createRandomLoc(int matrixWidth, int matrixHeight) { //(returns a random location within the game's matrix. (0,0) should be the first location),
             std::tuple<int, int> location = std::make_tuple(0,0);
-            int x = (rand() % (matrixWidth));
-            int y = (rand() % (matrixHeight));
             return location;
         }
 
